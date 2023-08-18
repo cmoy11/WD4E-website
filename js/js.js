853f2bb99d2d88fa3dcdc20f502383377521333c
Cameron Moy <97237787+cmoy11@users.noreply.github.com>
2023-08-18 10:49:13 -0400

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,21 +1,52 @@
 console.log("Connected");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+    scrollFunction();
+};
 
 function scrollFunction() {
-  let width = screen.width;
-  if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) && (width < 600)) {
-    console.log("scrolling")
-    document.querySelector('.WD4E').style.display = "flex";
-    document.querySelector('nav').style.position = "fixed";
-    document.querySelector('nav').style.top = "0px";
-  } else if ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) && (width > 600)) {
-    console.log("scrolling small")
-    document.querySelector('.WD4E').style.display = "none";
-    document.querySelector('nav').style.position = "fixed";
-    document.querySelector('nav').style.top = "0px";
-  } else {
-    document.querySelector(".WD4E").style.display = "flex";
-    document.querySelector('nav').style.position = "unset";
-  }
+    let width = screen.width;
+    let wd4eElement = document.querySelector('.WD4E');
+    let navElement = document.querySelector('nav');
+
+    if (document.documentElement.scrollTop > 20) {
+        navElement.style.position = "fixed";
+        navElement.style.top = "0";
+
+        if (width < 600) {
+            wd4eElement.style.display = "flex";
+        } else {
+            wd4eElement.style.display = "none";
+        }
+    } else {
+        wd4eElement.style.display = "flex";
+        navElement.style.position = "unset";
+    }
 }
+
+
+
+
+
+// console.log("Connected");
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   let width = screen.width;
+//   if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && (width < 600)) {
+//     console.log("scrolling")
+//     document.querySelector('.WD4E').style.display = "flex";
+//     document.querySelector('nav').style.position = "fixed";
+//     document.querySelector('nav').style.top = "0px";
+//   } else if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && (width > 600)) {
+//     console.log("scrolling small")
+//     document.querySelector('.WD4E').style.display = "none";
+//     document.querySelector('nav').style.position = "fixed";
+//     document.querySelector('nav').style.top = "0px";
+//   } else {
+//     document.querySelector(".WD4E").style.display = "flex";
+//     document.querySelector('nav').style.position = "unset";
+//   }
+// }
+
